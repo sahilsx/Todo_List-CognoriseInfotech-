3b2713627f83d38575a0d49627e8a7dc25fa8b93
sahil <itxsaaho@gmail.com>
2024-08-09 20:05:18 +0530

    submision commit

--- kimzim/src/App.jsx
+++ kimzim/src/App.jsx
@@ -74,7 +74,8 @@ function App() {
     < >
     <Navbar/> 
        <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[35%]">
-        <h1 className='font-bold text-center text-3xl'>KIMZIM - Manage your tasks at one place</h1>
+        <h1 className=' text-center text-3xl'>KIMZIM </h1>
+        <h1 className=' text-center text-3xl'>Manage Your Tasks At One Place </h1>
          <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className='text-2xl font-bold'>Add a Task</h2>
           <div className="flex">
